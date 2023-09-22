3dc20aca9843f3c5981713d0379fc62535645ebd
Hongbo Wen <hongbowen@ucsb.edu>
2023-09-21 18:29:59 -0700

    WIP: Partial Result

--- benchmarks/Discover/Discover_verify_halmos.t.sol
+++ benchmarks/Discover/Discover_verify_halmos.t.sol
@@ -3,18 +3,6 @@ pragma solidity ^0.8.10;
 import "./Discover.t.sol";
 
 contract DiscoverVerify is DiscoverTest {
-    function test_verify_cand001_0() public {
-        uint256 amt0 = 0x103f08008a00800000002;
-        uint256 amt1 = 0x3aa8259ef6e9da00005;
-        uint256 amt2 = 0x757d87e1e024a6ef59b2;
-        uint256 amt3 = 0x103f4673c028f142005dc;
-        borrow_usdt(amt0);
-        swap_ethpledge_usdt_disc(amt1);
-        swap_pair_disc_usdt(amt2);
-        payback_usdt(amt3);
-        require(attackGoal(), "Attack failed!");
-    }
-
     function test_verify_cand003_0() public {
         uint256 amt0 = 0x1000000000000000000001;
         uint256 amt1 = 0xeb817cba9d94bdeb2cc;
--- benchmarks/MUMUG/MUMUG_verify_halmos.t.sol
+++ benchmarks/MUMUG/MUMUG_verify_halmos.t.sol
@@ -4,10 +4,10 @@ import "./MUMUG.t.sol";
 
 contract MUMUGVerify is MUMUGTest {
     function test_verify_cand002_0() public {
-        uint256 amt0 = 0x8fc00030000000000000;
-        uint256 amt1 = 0x14c523338e64397ffffd;
-        uint256 amt2 = 0x2800000000000;
-        uint256 amt3 = 0x8fd019e3f66d33041826;
+        uint256 amt0 = 0x4eebf0000009c018800a;
+        uint256 amt1 = 0xf6e21b69e58819ed7c2;
+        uint256 amt2 = 0x57e130044480192f704;
+        uint256 amt3 = 0x4efc09b3f676f31c9830;
         borrow_mu(amt0);
         swap_pair_mu_usdce(amt1);
         swap_mubank_usdce_mu(amt2);
